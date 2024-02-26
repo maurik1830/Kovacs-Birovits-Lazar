506c9431e6c8c3dfad456d881cd05f2a6aadef9e
birovitsbence <100351280+birovitsbence@users.noreply.github.com>
2024-02-26 22:38:48 +0100

    Add files via upload
    birovits prog feladatok 3. projekthet kesz

--- Labiko/Labiko/Program.cs
+++ Labiko/Labiko/Program.cs
@@ -1,30 +1,94 @@
 ﻿// SZFT5 LáBiKo
-// Kovács Ádám Attila, Birovits Bence, Lázár Boldizsár, Szomor Levente, Nagy-Erdei Móric
+// Kovács Ádám Attila, Birovits Bence, Lázár Boldizsár
 
 using System;
 
 class Program
 {
     static char[,] palya;
-    static int palyaMeret = 15; // Növeltük a pálya méretét
+    static int palyaMeret = 20; // Növeltük a pálya méretét
     static int jatekosX, jatekosY;
 
     static void Main()
     {
-        //Inicializalas();   Birovits feladat
-        //KirajzolPalya();   Birovits feladat
+        Inicializalas();    // Birovits feladat
+        KirajzolPalya();    // Birovits feladat
 
         while (true)
         {
             Kezeles(Console.ReadKey(true).Key);
-            //KirajzolPalya();   Birovits feladat
+            KirajzolPalya();    // Birovits feladat
         }
     }
 
-    
+    static void Inicializalas()
+    {
+        palya = new char[palyaMeret, palyaMeret];
+        jatekosX = palyaMeret / 2;
+        jatekosY = palyaMeret / 2;
 
+        // Pálya feltöltése falakkal és üres területekkel // Lázár feladat
+        for (int i = 0; i < palyaMeret; i++)
+        {
+            for (int j = 0; j < palyaMeret; j++)
+            {
+                // Falak a pálya szélén
+                if (i == 0 || j == 0 || i == palyaMeret - 1 || j == palyaMeret - 1)
+                {
+                    palya[i, j] = 'X';
+                }
+                else
+                {
+                    palya[i, j] = '.';
+                }
+            }
+        }
 
+        // Játékos elhelyezése a pálya közepén
+        palya[jatekosX, jatekosY] = 'P';
 
+        // Tárgyak és élőlények elhelyezése
+        Random rand = new Random();
+        int targyakSzama = 8;
+        int elolenyekSzama = 5;
+
+        for (int i = 0; i < targyakSzama; i++)
+        {
+            int x, y;
+            do
+            {
+                x = rand.Next(1, palyaMeret - 1);
+                y = rand.Next(1, palyaMeret - 1);
+            } while (palya[x, y] != '.');
+
+            palya[x, y] = 'T';
+        }
+
+        for (int i = 0; i < elolenyekSzama; i++)
+        {
+            int x, y;
+            do
+            {
+                x = rand.Next(1, palyaMeret - 1);
+                y = rand.Next(1, palyaMeret - 1);
+            } while (palya[x, y] != '.');
+
+            palya[x, y] = 'E';
+        }
+    }
+
+    static void KirajzolPalya()
+    {
+        Console.Clear();
+        for (int i = 0; i < palyaMeret; i++)
+        {
+            for (int j = 0; j < palyaMeret; j++)
+            {
+                Console.Write(palya[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
 
     static void Kezeles(ConsoleKey key)
     {
@@ -63,10 +127,10 @@ class Program
 
     static void Mozgas(int dx, int dy)
     {
-        // Játékos mozgatása csak üres területekre vagy tárgyakra  Lázár feladat
+        // Játékos mozgatása csak üres területekre vagy tárgyakra Lázár feladat
         if (palya[jatekosX + dx, jatekosY + dy] != 'E' && palya[jatekosX + dx, jatekosY + dy] != 'X')
         {
-            palya[jatekosX, jatekosY] = ' ';
+            palya[jatekosX, jatekosY] = '.';
             jatekosX += dx;
             jatekosY += dy;
             palya[jatekosX, jatekosY] = 'P';
