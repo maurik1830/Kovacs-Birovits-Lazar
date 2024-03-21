3d395859d4c44313bf524b17b3403769bed3012a
maurik1830 <nagyemoric@gmail.com>
2024-03-21 14:12:53 +0100

    +palyak

---
+++ Labiko/Labiko/Courts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaBiKo
+{
+    internal class Palya1
+    {
+        private int szoba1mag;
+        private int szoba1szel;
+
+        private int szoba2mag;
+        private int szoba2szel;
+
+        private int szoba3mag;
+        private int szoba3szel;
+
+        private int szoba4mag;
+        private int szoba4szel;
+    }
+}
